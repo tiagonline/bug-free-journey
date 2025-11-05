38f283b7f0b28a45db49fd1400f1e7c6561711d8
Tiago Oliveira Silva <tiago.silva@minu.co>
2025-11-05 15:01:16 -0300

    Ajuste do teste de carga

--- automacao/k6/scripts/upload_stress_test.js
+++ automacao/k6/scripts/upload_stress_test.js
@@ -14,15 +14,16 @@ import http from 'k6/http';
 import { check, sleep } from 'k6';
 import { SharedArray } from 'k6/data';
 
-// --- ARQUIVO DE TESTE (MOCKADO) ---
-// Como não temos um arquivo real, criamos um binário fictício
+// Criação do arquivo de teste (MOCKADO)
+// Como não temos um arquivo real, criei um binário fictício
 // para simular o payload de um arquivo de 5MB.
+// O arquivo é criado uma única vez e compartilhado entre os VUs.
 // (Em um cenário real, poderíamos carregar um arquivo do disco)
 const fileBin = new SharedArray('mockFile', function () {
-    return [new Array(5 * 1024 * 1024).join('a')]; // 5MB de 'a'
+    return [new Array(5 * 1024 * 1024).join('a')]; // Cria um 5MB cheio de "a's"
 });
 
-// --- OPÇÕES DE EXECUÇÃO (Teste de Carga) ---
+// Configuração do teste de carga
 export const options = {
     // 'stages' define o ramp-up e ramp-down de usuários virtuais (VUs)
     stages: [
@@ -32,34 +33,33 @@ export const options = {
         { duration: '2m', target: 100 }, // 4. Mantém 100 VUs por 2 minutos (Stress)
         { duration: '1m', target: 0 },   // 5. Rampa de 100 a 0 VUs em 1 minuto
     ],
-    // 'thresholds' definem os SLAs (critérios de aceite)
+    // Aqui se define os SLAs, critérios de aceite para o teste
     thresholds: {
         'http_req_failed': ['rate<0.01'],    // Taxa de falha HTTP deve ser < 1%
-        'http_req_duration{p(95)}': ['p(95)<3000'], // 95% das requisições devem ser < 3s
+        'http_req_duration': ['p(95)<3000'], // 95% das requisições devem ser < 3s
     },
 };
 
-// --- FUNÇÃO PRINCIPAL (O TESTE) ---
+//Execução do teste
 export default function () {
-    
+
     // A URL da API é fictícia, pois o desafio é "cego".
-    const url = 'https://api.test.sigga/upload'; 
+    const url = 'https://api.test.sigga/upload';
 
     // Pega o binário do arquivo
     const fileData = fileBin[0];
-    
+
     // Monta o payload como 'multipart/form-data'
     const data = {
         // O nome 'file' é o 'name' esperado pelo input no backend
-        file: http.file(fileData, 'documento_de_teste.pdf', 'application/pdf'), 
-        // (Opcional) Enviar outros dados do formulário
+        file: http.file(fileData, 'documento_de_teste.pdf', 'application/pdf'),
         documentType: 'Invoice',
     };
 
-    // --- REQUISIÇÃO HTTP ---
+    // Requisição HTTP ---
     const res = http.post(url, data);
 
-    // --- ASSERTIVAS (Checks) ---
+    // Validações
     check(res, {
         'API de Upload retornou status 201 (Created)': (r) => r.status === 201,
         'API de Upload retornou corpo (body) com sucesso': (r) => r.body.includes('success: true'),
